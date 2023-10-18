28594f682357239287b1102172b71f81eaa16b21
Niakr1s <pavel2188@gmail.com>
2023-10-18 16:45:54 +0300

    solved aoc-2015-day10-part2

--- aoc-2015/aoc-2015-day10/src/main.rs
+++ aoc-2015/aoc-2015-day10/src/main.rs
@@ -2,9 +2,15 @@ use aoc_2015_day10::look_and_say::look_and_say_n_times;
 
 fn main() {
     run_part1();
+    run_part2();
 }
 
 fn run_part1() {
     let res = look_and_say_n_times("1113222113", 40);
     println!("Part 1: {}", res.len());
+}
+
+fn run_part2() {
+    let res = look_and_say_n_times("1113222113", 50);
+    println!("Part 2: {}", res.len());
 }
