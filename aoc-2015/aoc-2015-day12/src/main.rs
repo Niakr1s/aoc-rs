f383b586482f2c73886b9e9d30c32720b94ab0f3
Niakr1s <pavel2188@gmail.com>
2023-10-21 02:56:40 +0300

    trying to make part2 done, closures problem

--- aoc-2015/aoc-2015-day12/src/main.rs
+++ aoc-2015/aoc-2015-day12/src/main.rs
@@ -6,8 +6,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let json: serde_json::Value = serde_json::from_reader(file)?;
 
-    let c = <serde_json::Value as Summarize>::count(&json)?;
-    println!("Part 1: {}", c);
+    // let part1_res = <serde_json::Value as Summarize>::count(&json)?;
+    // println!("Part 1: {}", part1_res);
+
+    let mut called = 0;
+    let part2_res = json.count_if(move |v| {
+        // let mut should_skip = false;
+        // if let serde_json::Value::Object(o) = v {
+        //     should_skip = o.values().any(|v| {
+        //         if let serde_json::Value::String(s) = v {
+        //             s == "red"
+        //         } else {
+        //             false
+        //         }
+        //     });
+        // }
+        // called += 1;
+        // println!("Should skip = {should_skip}");
+        // !should_skip
+        false
+    })?;
+    println!("Part 2: {}", part2_res);
+    println!("called {called} times");
 
     Ok(())
 }
--- aoc-2015/aoc-2015-day12/src/summarize.rs
+++ aoc-2015/aoc-2015-day12/src/summarize.rs
@@ -2,6 +2,10 @@ pub trait Summarize {
     type Err;
 
     fn count(&self) -> Result<i64, Self::Err>;
+
+    fn count_if<F>(&self, p: F) -> Result<i64, Self::Err>
+    where
+        F: FnMut(&Self) -> bool;
 }
 
 #[derive(Debug, thiserror::Error)]
@@ -13,7 +17,7 @@ pub enum JsonSummarizeError {
 impl Summarize for serde_json::Value {
     type Err = JsonSummarizeError;
 
-    fn count(&self) -> Result<i64, JsonSummarizeError> {
+    fn count(&self) -> Result<i64, Self::Err> {
         fn count_values<'a>(
             nums: impl Iterator<Item = &'a serde_json::Value>,
         ) -> Result<i64, JsonSummarizeError> {
@@ -36,4 +40,43 @@ impl Summarize for serde_json::Value {
         };
         Ok(ret)
     }
+
+    fn count_if<F>(&self, mut p: F) -> Result<i64, Self::Err>
+    where
+        F: FnMut(&Self) -> bool,
+    {
+        if !p(&self) {
+            return Ok(0);
+        }
+
+        let ret = match self {
+            serde_json::Value::Number(n) => n.as_i64().ok_or(JsonSummarizeError::NumberError)?,
+            serde_json::Value::Array(v) => count_values(v.iter(), &mut p)?,
+            serde_json::Value::Object(o) => count_values(o.values(), &mut p)?,
+            serde_json::Value::Null | serde_json::Value::Bool(_) | serde_json::Value::String(_) => {
+                0
+            }
+        };
+
+        Ok(ret)
+        // Ok(0)
+    }
+}
+
+fn count_values<'a, F>(
+    nums: impl Iterator<Item = &'a serde_json::Value>,
+    mut p: F,
+) -> Result<i64, JsonSummarizeError>
+where
+    F: FnMut(&serde_json::Value) -> bool,
+{
+    let ret = nums.filter(|&v| p(v)).collect::<Vec<_>>();
+    // .map(|v| v.count())
+    let ret = ret
+        .into_iter()
+        .map(|v| v.count_if(&mut p))
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter()
+        .sum();
+    Ok(ret)
 }
