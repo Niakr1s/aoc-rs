cd8b8a4c150db16fae24036a27f8bbc942d58407
Niakr1s <pavel2188@gmail.com>
2023-10-21 18:58:54 +0300

    fast fix

--- aoc-2015/aoc-2015-day13/src/main.rs
+++ aoc-2015/aoc-2015-day13/src/main.rs
@@ -1,7 +1,6 @@
 use std::io::BufRead;
 
 use aoc_2015_day13::relations::{Relation, Relations};
-use itertools::Itertools;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let filepath = helpers::get_filepath_from_args();
@@ -11,28 +10,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut relations = Relations::new();
     for line in lines {
         let line = line?;
-        let relation = line_to_relation(&line)?;
+        let relation = Relation::from_adventofcode_line(&line)?;
         relations.update_relation(relation);
     }
     Ok(())
-}
-
-/// Alice would gain 54 happiness units by sitting next to Bob.
-fn line_to_relation(mut line: &str) -> Result<Relation<&str, u8>, &'static str> {
-    if line.ends_with(".") {
-        line = &line[..line.len() - 1];
-    }
-
-    let v = line.split_whitespace().collect_vec();
-
-    let (to, happiness, from) = (0, 3, 10);
-    if v.len() != 11 {
-        return Err("invalid line");
-    } else {
-        Ok(Relation {
-            from: v[from],
-            to: v[to],
-            happiness: v[happiness].parse().unwrap(),
-        })
-    }
 }
--- aoc-2015/aoc-2015-day13/src/relations.rs
+++ aoc-2015/aoc-2015-day13/src/relations.rs
@@ -1,20 +1,19 @@
 use std::collections::HashMap;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, derive_more::From)]
-pub struct Happiness(u8);
+#[derive(
+    Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, derive_more::From, derive_more::Deref,
+)]
+pub struct Happiness(i32);
+
+#[derive(Debug)]
+pub struct Relation<T> {
+    pub from: T,
+    pub to: T,
+    pub happiness: Happiness,
+}
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Idx(usize);
-
-#[derive(Debug)]
-pub struct Relation<T, H>
-where
-    H: Into<Happiness>,
-{
-    pub from: T,
-    pub to: T,
-    pub happiness: H,
-}
 
 #[derive(Debug)]
 pub struct Relations {
@@ -32,17 +31,15 @@ impl Relations {
         }
     }
 
-    pub fn update_relation<S, H>(&mut self, relation: Relation<S, H>)
+    pub fn update_relation<S>(&mut self, relation: Relation<S>)
     where
         S: AsRef<str>,
-        H: Into<Happiness>,
     {
         let Relation {
             from,
             to,
             happiness,
         } = relation;
-        let happiness: Happiness = happiness.into();
         let from = self.try_add_participant(from.as_ref());
         let to = self.try_add_participant(to.as_ref());
 
@@ -64,5 +61,78 @@ impl Relations {
 
     fn incr_next_idx(&mut self) {
         self.next_idx = Idx(self.next_idx.0 + 1);
+    }
+}
+
+pub mod relation {
+    use itertools::Itertools;
+
+    use super::*;
+
+    #[derive(Debug, thiserror::Error)]
+    pub enum IntoRelationError {
+        #[error("invalid line length")]
+        InvalidLineLength,
+        #[error("invalid happiness sign")]
+        InvalidHappinessSign,
+        #[error("invalid happiness value")]
+        InvalidHappinessValue,
+    }
+
+    impl Relation<&str> {
+        /// Example line:
+        ///
+        /// Alice would gain 54 happiness units by sitting next to Bob.
+        pub fn from_adventofcode_line(mut line: &str) -> Result<Relation<&str>, IntoRelationError> {
+            if line.ends_with(".") {
+                line = &line[..line.len() - 1];
+            }
+
+            let words = line.split_whitespace().collect_vec();
+
+            let (to, happiness_sign, happiness, from) = (0, 2, 3, 10);
+            if words.len() != 11 {
+                return Err(IntoRelationError::InvalidLineLength);
+            } else {
+                let happiness_sign = match words[happiness_sign] {
+                    "gain" => 1,
+                    "lose" => -1,
+                    _ => return Err(IntoRelationError::InvalidHappinessSign),
+                };
+                let happiness: Happiness = words[happiness]
+                    .parse::<i32>()
+                    .map(|x| x * happiness_sign)
+                    .map_err(|_| IntoRelationError::InvalidHappinessValue)?
+                    .into();
+                Ok(Relation {
+                    from: words[from],
+                    to: words[to],
+                    happiness,
+                })
+            }
+        }
+    }
+    #[cfg(test)]
+
+    mod from_adventofcode_line {
+        use super::*;
+
+        #[test]
+        fn alice_from_bob_plus_happiness() {
+            let line = "Alice would gain 54 happiness units by sitting next to Bob.";
+            let relation = Relation::from_adventofcode_line(line).unwrap();
+            assert_eq!(relation.to, "Alice");
+            assert_eq!(relation.from, "Bob");
+            assert_eq!(*relation.happiness, 54);
+        }
+
+        #[test]
+        fn alice_from_bob_minus_happiness() {
+            let line = "Alice would lose 54 happiness units by sitting next to Bob.";
+            let relation = Relation::from_adventofcode_line(line).unwrap();
+            assert_eq!(relation.to, "Alice");
+            assert_eq!(relation.from, "Bob");
+            assert_eq!(*relation.happiness, -54);
+        }
     }
 }
