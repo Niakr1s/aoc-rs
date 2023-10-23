54ad6f7f14a6e930ee96ee7212e3b3883db205e4
Niakr1s <pavel2188@gmail.com>
2023-10-23 19:13:17 +0300

    aoc-2015-day16 init and completed AuntSue and FromStr -> AuntSue

---
+++ aoc-2015/aoc-2015-day16/src/lib.rs
@@ -0,0 +1,75 @@
+pub mod aunt_sue {
+    use std::collections::HashMap;
+
+    pub struct AuntSue {
+        pub no: u32,
+        pub facts: HashMap<String, u32>,
+    }
+
+    #[derive(Debug, thiserror::Error)]
+    pub enum ParseError {
+        #[error("Invalid input: {0}")]
+        InvalidStr(&'static str),
+        #[error("ParseIntError")]
+        ParseIntError(#[from] std::num::ParseIntError),
+    }
+
+    impl std::str::FromStr for AuntSue {
+        type Err = ParseError;
+
+        /// Example:
+        ///
+        /// Sue 25: goldfish: 10, trees: 8, perfumes: 6
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut iter = s.split_whitespace();
+
+            let no = iter
+                .by_ref()
+                .skip(1)
+                .next()
+                .ok_or(ParseError::InvalidStr("len < 2"))?
+                .trim_end_matches(':')
+                .parse::<u32>()?;
+
+            let mut facts = HashMap::new();
+
+            while let Some(fact) = iter.next() {
+                let fact = fact.trim_end_matches(':');
+                let amount = iter
+                    .next()
+                    .ok_or(ParseError::InvalidStr("no value for fact"))?
+                    .trim_end_matches(',')
+                    .parse::<u32>()?;
+
+                facts.insert(fact.to_owned(), amount);
+            }
+
+            Ok(AuntSue { no, facts })
+        }
+    }
+
+    #[cfg(test)]
+    #[allow(unused_imports)]
+    mod tests {
+        use super::*;
+
+        mod aunt_sue {
+            use super::*;
+
+            mod from_str {
+                use super::*;
+
+                #[test]
+                fn works() {
+                    let input = "Sue 1: cars: 9, akitas: 3, goldfish: 0";
+                    let sue = input.parse::<AuntSue>().unwrap();
+                    assert_eq!(sue.no, 1);
+                    assert_eq!(sue.facts.len(), 3);
+                    assert_eq!(sue.facts["cars"], 9);
+                    assert_eq!(sue.facts["akitas"], 3);
+                    assert_eq!(sue.facts["goldfish"], 0);
+                }
+            }
+        }
+    }
+}
