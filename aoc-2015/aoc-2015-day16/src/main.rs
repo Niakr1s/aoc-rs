6eb9f484760ba5138bf660bbaba1c96ba1f21333
Niakr1s <pavel2188@gmail.com>
2023-10-23 19:49:38 +0300

    solved aoc-2015-day16-part1

--- aoc-2015/aoc-2015-day16/src/main.rs
+++ aoc-2015/aoc-2015-day16/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::{collections::HashMap, io::BufRead, str::FromStr};
+
+use aoc_2015_day16::aunt_sue::{facts::Facts, AuntSue};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filepath = helpers::get_filepath_from_args();
+    let file = std::fs::File::open(filepath)?;
+    let reader = std::io::BufReader::new(file);
+
+    let aunts = reader
+        .lines()
+        .flat_map(|l| l.map(|l| AuntSue::from_str(&l)))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let known_facts = Facts(HashMap::from([
+        ("children".to_owned(), 3),
+        ("cats".to_owned(), 7),
+        ("samoyeds".to_owned(), 2),
+        ("pomeranians".to_owned(), 3),
+        ("akitas".to_owned(), 0),
+        ("vizslas".to_owned(), 0),
+        ("goldfish".to_owned(), 5),
+        ("trees".to_owned(), 3),
+        ("cars".to_owned(), 2),
+        ("perfumes".to_owned(), 1),
+    ]));
+
+    let possible_aunts = aunts
+        .iter()
+        .filter(|&aunt| known_facts.possible_eq(&aunt.facts))
+        .map(|aunt| aunt.no)
+        .collect::<Vec<_>>();
+
+    println!("{:?}", possible_aunts);
+
+    Ok(())
 }
