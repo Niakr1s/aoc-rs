fc9fc18c5cd4d31c77704627d327bb570ec61444
Niakr1s <pavel2188@gmail.com>
2023-10-23 12:24:48 +0300

    finished aoc-2015-day15-part1

--- aoc-2015/aoc-2015-day15/src/main.rs
+++ aoc-2015/aoc-2015-day15/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::BufRead;
+
+use aoc_2015_day15::{Cookie, Ingredient};
+use itertools::Itertools;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filepath = helpers::get_filepath_from_args();
+    let f = std::fs::File::open(filepath)?;
+    let reader = std::io::BufReader::new(f);
+
+    let ingridients = reader
+        .lines()
+        .flat_map(|l| l.map(|l| l.parse::<Ingredient>()))
+        .collect::<Result<Vec<Ingredient>, _>>()?;
+
+    let max = (0..ingridients.len())
+        .combinations_with_replacement(100)
+        .map(|c| {
+            let ingridients = c
+                .into_iter()
+                .counts()
+                .into_iter()
+                .map(|c| (ingridients[c.0].clone(), c.1 as u32))
+                .collect::<Vec<_>>();
+            let cookie = Cookie::new(ingridients);
+            cookie.score_without_calories()
+        })
+        .max();
+    println!("Max score without calories: {:?}", max);
+
+    Ok(())
 }
