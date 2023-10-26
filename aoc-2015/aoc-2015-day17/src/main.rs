0f29909cd276f8650aee75c8395ea155b8f611fd
Niakr1s <pavel2188@gmail.com>
2023-10-26 16:25:28 +0300

    solved aoc2015-day17-part2

--- aoc-2015/aoc-2015-day17/src/main.rs
+++ aoc-2015/aoc-2015-day17/src/main.rs
@@ -7,14 +7,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = std::fs::File::open(filepath)?;
     let containers = get_numbers(file)?;
 
-    let combinations = containers
-        .into_iter()
-        .powerset()
-        .filter(|c| c.iter().sum::<u32>() == 150);
-    let count = combinations.count();
-    println!("Part 1: {}", count);
+    println!("Part 1: {}", combinations_count(&containers));
+    println!("Part 2: {}", different_min_ways_count(&containers));
 
     Ok(())
+}
+
+fn combinations_count(containers: &[u32]) -> usize {
+    containers
+        .iter()
+        .powerset()
+        .filter(|c| c.into_iter().copied().sum::<u32>() == 150)
+        .count()
+}
+
+fn different_min_ways_count(containers: &[u32]) -> usize {
+    containers
+        .iter()
+        .powerset()
+        .filter(|c| c.into_iter().copied().sum::<u32>() == 150)
+        .min_set_by(|a, b| a.len().cmp(&b.len()))
+        .len()
 }
 
 fn get_numbers(input: impl std::io::Read) -> Result<Vec<u32>, Box<dyn std::error::Error>> {
