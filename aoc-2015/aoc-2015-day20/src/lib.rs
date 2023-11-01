ea4a47ae0a8119ae7fc5c403d62848b02d81d7dd
Niakr1s <pavel2188@gmail.com>
2023-11-01 19:44:08 +0300

    refactored part1 to be ultra fast + solved part2 (it doesn't works though)

--- aoc-2015/aoc-2015-day20/src/lib.rs
+++ aoc-2015/aoc-2015-day20/src/lib.rs
@@ -6,11 +6,22 @@ pub struct HouseWithPresents {
 
 pub struct PresentsIter {
     next_house: u32,
+    deliver: fn(u32) -> u64,
 }
 
-impl Default for PresentsIter {
-    fn default() -> Self {
-        Self { next_house: 1 }
+impl PresentsIter {
+    pub fn new_deliver1() -> Self {
+        Self {
+            next_house: 1,
+            deliver: deliver1,
+        }
+    }
+
+    pub fn new_deliver2() -> Self {
+        Self {
+            next_house: 1,
+            deliver: deliver2,
+        }
     }
 }
 
@@ -20,22 +31,43 @@ impl Iterator for PresentsIter {
     fn next(&mut self) -> Option<Self::Item> {
         let house = self.next_house;
         self.next_house += 1;
-        let amount = Some(presents_for_house(house));
-        amount.map(|amount| HouseWithPresents { house, amount })
+        let amount = (self.deliver)(house);
+        Some(HouseWithPresents { house, amount })
     }
 }
 
-fn presents_for_house(house: u32) -> u64 {
-    let elves = 1..=house;
-    elves.map(|elf| presents_for_house_by_elf(house, elf)).sum()
+fn deliver1(house: u32) -> u64 {
+    let elves = find_divisors(house);
+    elves.into_iter().map(|elf| elf as u64 * 10).sum()
 }
 
-fn presents_for_house_by_elf(house: u32, elf: u32) -> u64 {
-    if house % elf == 0 {
-        elf as u64 * 10
-    } else {
-        0
+fn deliver2(house: u32) -> u64 {
+    // we won't use find_divisors function, because it's not more than 50 elves
+    let start_elf = house.checked_sub(50).unwrap_or(0) + 1;
+    let elves = start_elf..=house;
+    elves
+        .map(|elf| if house % elf == 0 { elf as u64 * 11 } else { 0 })
+        .sum()
+}
+
+fn find_divisors(n: u32) -> Vec<u32> {
+    let mut divisors = Vec::new();
+
+    let sqrt_n = (n as f64).sqrt() as u32;
+
+    for i in 1..=sqrt_n {
+        if n % i == 0 {
+            divisors.push(i);
+
+            if n / i != i {
+                divisors.push(n / i);
+            }
+        }
     }
+
+    divisors.sort();
+
+    divisors
 }
 
 #[cfg(test)]
@@ -43,8 +75,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn presents_works() {
-        let mut presents = PresentsIter::default();
+    fn presents_deliver1_works() {
+        let mut presents = PresentsIter::new_deliver1();
 
         let want_presents = [10, 30, 40, 70, 60, 120, 80, 150, 130];
 
@@ -54,5 +86,39 @@ mod tests {
                 HouseWithPresents { house, amount }
             );
         }
+    }
+
+    #[test]
+    fn presents_deliver2_works() {
+        let mut presents = PresentsIter::new_deliver2();
+
+        let want_presents = [11, 33, 44, 77, 66, 132, 88, 165, 143];
+
+        for (&amount, house) in want_presents.iter().zip(1..) {
+            assert_eq!(
+                presents.next().unwrap(),
+                HouseWithPresents { house, amount }
+            );
+        }
+
+        let house51 = presents
+            .by_ref()
+            .skip_while(|house| house.house < 51)
+            .next()
+            .unwrap();
+
+        assert_eq!(house51.house, 51);
+        assert_eq!(house51.amount, (3 + 17 + 51) * 11);
+
+        let house52 = presents.by_ref().next().unwrap();
+        assert_eq!(house52.house, 52);
+        assert_eq!(house52.amount, (4 + 13 + 26 + 52) * 11);
+    }
+
+    #[test]
+    fn find_divisors_works() {
+        assert_eq!(find_divisors(1), [1]);
+        assert_eq!(find_divisors(51), [1, 3, 17, 51]);
+        assert_eq!(find_divisors(52), [1, 2, 4, 13, 26, 52]);
     }
 }
--- aoc-2015/aoc-2015-day20/src/main.rs
+++ aoc-2015/aoc-2015-day20/src/main.rs
@@ -1,16 +1,32 @@
 use aoc_2015_day20::PresentsIter;
 
-fn main() {
-    let presents = PresentsIter::default();
+const USAGE: &str = "Usage: aoc-2015-day20 part1|part2";
 
-    let house = presents
-        // .skip(1)
-        // .step_by(2)
-        // .take(20)
+fn main() {
+    let part = std::env::args().skip(1).next().expect(USAGE);
+    if part.ends_with("1") {
+        part1();
+    } else if part.ends_with("2") {
+        part2();
+    } else {
+        panic!("{USAGE}");
+    }
+}
+
+fn part1() {
+    println!("Running part 1...");
+    let house = PresentsIter::new_deliver1()
         .skip_while(|house| house.amount < 36000000)
-        // .for_each(|house| println!("{house:?}"));
         .next()
         .unwrap();
-
     println!("Part 1: {house:?}");
+}
+
+fn part2() {
+    println!("Running part 2...");
+    let house = PresentsIter::new_deliver2()
+        .skip_while(|house| house.amount < 36000000)
+        .next()
+        .unwrap();
+    println!("Part 2: {house:?}");
 }
